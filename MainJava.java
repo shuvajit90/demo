94604aae431dc4616ab12e1311dbe2c486a54e08
Shuvajit Dutta <shuvajitduttawork@gmail.com>
2018-05-26 16:59:37 +0530

    some release commits

--- MainJava.java
+++ MainJava.java
@@ -9,5 +9,6 @@ public class MainJava {
 		System.out.println("new collaborator user == shuvajituser90");
 		System.out.println("feature branch for user shuvajit90 == feature1");
 		System.out.println("some commit to feature branch to demonstrate git rebase");
+		#Few lines of code
 	}
 }
