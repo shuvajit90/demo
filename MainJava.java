fcd58f2b035d04fa524be7107f78bb3da39964fe
Shuvajit Dutta <shuvajitduttawork@gmail.com>
2018-05-26 11:51:21 +0530

    Second commit by user shuvajit90 @20180526

---
+++ MainJava.java
@@ -0,0 +1,10 @@
+package com.practice;
+
+public class MainJava {
+	
+	public static void main() {
+		HelloWorld hwobj = new HelloWorld();
+		System.out.println(hwobj.Message());
+		System.out.println("user == shuvajit90");
+	}
+}
